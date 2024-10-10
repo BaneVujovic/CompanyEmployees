ec83801a0adaeb441c6a022d5100ee2e61fa5d3d
Mup <banejole@gmail.com>
2024-10-10 08:18:35 +0200

    Data Shaping part II

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Options;
 using NLog;
+using Service.DataShaping;
+using Shared.DataTransferObject;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +27,7 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 });
 
 builder.Services.AddScoped<ValidationFilterAttribute>();
+builder.Services.AddScoped<IDataShaper<EmployeeDto>, DataShaper<EmployeeDto>>();
 
 builder.Services.AddControllers(config => {
     config.RespectBrowserAcceptHeader = true;
